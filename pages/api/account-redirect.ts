7077613612ab2ba0adfd2fb853d2bd38328502b8
williamherring <william.herring.au@gmail.com>
2024-09-03 17:35:09 +1000

    continued work on wca oauth

--- pages/api/account-redirect.ts
+++ pages/api/account-redirect.ts
@@ -3,6 +3,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const redirect = req.query.redirect || '/my-competitions'
     const code = req.query.code
+    console.log(code)
     const data = await fetch('https://www.worldcubeassociation.org/oauth/token', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
@@ -13,6 +14,16 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             'client_secret': process.env.WCA_CLIENT_SECRET,
             'redirect_uri': process.env.BASE_URL! + redirect,
         })
-    }).then((res) => res.json())
+    }).then((r) => r.json())
+    console.log(data)
     const token = data['access_token']
+
+    const userData = await fetch('https://www.worldcubeassociation.org/api/v0/me', {
+        method: 'GET',
+        headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${token}`},
+    }).then(r => r.json())
+
+    console.log(userData)
+
+    return res.redirect(307, redirect as string)
 }
