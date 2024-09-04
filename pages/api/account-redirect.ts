554943a0a51867025f4e874d2100803a9f052de6
William Herring <73984595+william-herring@users.noreply.github.com>
2024-09-05 08:00:56 +1000

    session-setting with user generation

--- pages/api/account-redirect.ts
+++ pages/api/account-redirect.ts
@@ -1,17 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { getIronSession } from 'iron-session'
-import { cookies } from "next/headers";
+import prisma from "@/lib/prisma";
 
 type SessionData = {
-    password: string;
-    cookieName: string;
-    user: {
-        id: number;
-        wcaId: string;
-        name: string;
-        avatar: string;
-        isDelegate: boolean;
-    }
+    id: number;
+    wcaId: string | null;
+    name: string;
+    avatar: string;
+    isDelegate: boolean;
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
@@ -30,13 +26,37 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     }).then((r) => r.json())
     const token = data['access_token']
 
-    const session = await getIronSession<SessionData>(cookies(), { })
-    // res.setHeader('Set-Cookie', `token=${token}; HttpOnly; Secure`);
-
     const userData = await fetch('https://www.worldcubeassociation.org/api/v0/me', {
         method: 'GET',
         headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${token}`},
     }).then(r => r.json())
+
+    const user = await prisma.user.findUnique({
+        where: {
+            wcaId: userData['me']['wca_id']
+        }
+    }) || await prisma.user.create({
+        data: {
+            wcaId: userData['me']['wca_id'],
+            name: userData['me']['name'],
+            avatar: userData['me']['avatar']['thumb_url'],
+            delegate: userData['me']['delegate_status'] != null,
+        }
+    })
+
+    const session = await getIronSession<SessionData>(req, res, {
+        password: token,
+        cookieName: 'user',
+        cookieOptions: {
+            secure: process.env.NODE_ENV === "production",
+        }
+    })
+    session.id = user.id
+    session.wcaId = user.wcaId
+    session.name = user.name!
+    session.avatar = user.avatar!
+    session.isDelegate = user.delegate
+    await session.save()
 
     return res.redirect(307, redirect as string)
 }
